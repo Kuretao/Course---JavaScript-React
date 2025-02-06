af850befde184cd7402d2da9fef6bf47e3a318de
Kuretao <frolov.bogdann@yandex.com>
2025-02-06 15:23:44 +0300

    Upload commit

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let storeName = 'store';
+const storeDescription = {
+    budget: 10000,
+    employees: ['Bogdan', 'Misha', 'Denis'],
+    products: {
+        apple: 150,
+        tomato: 200
+    },
+    open: false,
+}
